00bf6d7f34bc22a9df2ca886979c9d767bd62800
alxwtf <alxwtf@gmail.com>
2017-11-05 10:46:13 +0300

    2

---
+++ LinqDB/Base.cs
@@ -0,0 +1,47 @@
+﻿namespace LinqDB
+{
+    public abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string post;
+        public int salary;
+        public int exWorks;
+    }
+
+    class Employee1 : Person
+    {
+        public Employee1()
+        {
+            FirstName = "Дмитрий";
+            LastName = "Васильев";
+            post = "Старший продавец";
+            salary = 30000;
+            exWorks = 20;
+        }
+    }
+
+    class Employee2 : Person
+    {
+        public Employee2()
+        {
+            FirstName = "Василий";
+            LastName = "Деревенькин";
+            post = "Продавец";
+            salary = 22000;
+            exWorks = 50;
+        }
+    }
+
+    class Employee3 : Person
+    {
+        public Employee3()
+        {
+            FirstName = "Алексей";
+            LastName = "Горбунов";
+            post = "Продавец";
+            salary = 20000;
+            exWorks = 80;
+        }
+    }
+}
--- LinqDB/Program.cs
+++ LinqDB/Program.cs
@@ -10,6 +10,18 @@ namespace LinqDB
     {
         static void Main(string[] args)
         {
+            var workers = new List<Person> {
+                new Employee1(),
+                new Employee2(),
+                new Employee3()
+            };
+            var query = (from t in workers
+                        where t.exWorks == workers.Max(x => x.exWorks)
+                        select new { t.FirstName, t.LastName }).Single();
+            Console.WriteLine($"Средняя з/п:{workers.Average(x => x.salary)}");
+            Console.WriteLine($"Кол-во работяг:{workers.Count}");
+            
+            Console.WriteLine($"{query.FirstName+" "+query.LastName} продал больше всех, ему полагается премия!!!");
         }
     }
 }
